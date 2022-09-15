1631f17ea0b51111d41ab93d29116e1e5641b1a7
Amlan-Nayak <nayak.amlan69@gmail.com>
2022-09-15 16:18:34 +0200

    Added & Modified a general boris processing py file

--- Boris_Processing.py
+++ Boris_Processing.py
@@ -1,29 +1,37 @@
 import numpy as np
 import pandas as pd
+import os
+from pathlib import Path
 
+import config
 
-path = '/media/amlan/Data/Thesis Data/BORIS Data/NQ_2021_1/Event Data/'
-file = 'NQ_VIDEO_BEHAVIOUR_20210811_VNQM011_Aggregated.csv'
+MainPath = config.BorisPath
+SavePath = config.SavePath
+GROUPS = config.GROUPS
+AddPath = config.BorisAddPath
 
-Data = pd.read_csv(path + file, usecols = ['Total length', 'Behavior','Start (s)', 'Stop (s)', 'Duration (s)', 'Video Seconds Of Timestamp','Timestamp'])
+For k in GROUPS:
+    paths = os.listdir(MainPath + str(k) + AddPath)
+    paths = [i for i in paths if 'Aggregated' in i]
+    for file in paths:
+        Data = pd.read_csv(MainPath + str(k) + AddPath + file, usecols = ['Total length', 'Behavior','Start (s)', 'Stop (s)', 'Duration (s)', 'Video Seconds Of Timestamp','Timestamp'])
+        Data = Data.loc[Data['Duration (s)']>1]
 
-Data = Data.loc[Data['Duration (s)']>1]
+        time = pd.to_datetime(Data.Timestamp.loc[0])
+        day = file.split('_')[3]
+        audit_start_time = time.replace(year=int(day[0:4]), month=int(day[4:6]), day=int(day[6:8]))
+        vid_start_time = Data['Video Seconds Of Timestamp'].loc[0]
 
-time = pd.to_datetime(Data.Timestamp.loc[0])
-day = file.split('_')[3]
-audit_start_time = time.replace(year=int(day[0:4]), month=int(day[4:6]), day=int(day[6:8]))
-vid_start_time = Data['Video Seconds Of Timestamp'].loc[0]
+        starts = np.ceil(Data['Start (s)'] - vid_start_time).astype(int)
+        starts[starts<0] = 0
+        stops = np.floor(Data['Stop (s)'] - vid_start_time).astype(int)
 
-starts = np.ceil(Data['Start (s)'] - vid_start_time).astype(int)
-starts[starts<0] = 0
-stops = np.floor(Data['Stop (s)'] - vid_start_time).astype(int)
+        times,labels =[],[]
+        for i in range(len(starts)):
+            for j in range(starts[i],stops[i]):
+                times.append(audit_start_time + pd.Timedelta(seconds=j))
+                labels.append(Data.loc[starts == starts[i],'Behavior'].values[0])
 
-times,labels =[],[]
-for i in range(len(starts)):
-    for j in range(starts[i],stops[i]):
-        times.append(audit_start_time + pd.Timedelta(seconds=j))
-        labels.append(Data.loc[starts == starts[i],'Behavior'].values[0])
+        Boris = pd.DataFrame(zip(times,labels),columns=['Timestamp','Behavior'])
 
-Boris = pd.DataFrame(zip(times,labels),columns=['Timestamp','Behavior'])
-
-Boris.to_csv(file.split('Aggregated')[0] + 'labels' + '.csv')
+        Boris.to_csv(SavePath + str(k) + '/' + file.split('Aggregated')[0] + 'labels.csv')
--- config.py
+++ config.py
@@ -2,12 +2,22 @@
 
 GROUPS = ['ZU_2021_1','ZU_2021_2','NQ_2021_1','RW_2021_1']
 
+#For GPS_Extraction.py
 MainPath = '/media/amlan/Data/Thesis Data/Raw Data/' #Directory 
 SavePath = '/media/amlan/Data/Thesis Data/Processed Data/' 
 AddPath = '/COLLAR/GPS/' #For my database structure, this was need. 
 
+
+
+#For ACC_Extraction.py
 FREQUENCY =  10 #Frequency of the ACC files
-WINDOW_SIZE = 20 #If frequency is n, window size can be n, 2n , 3n etc 
+WINDOW_SIZE = 20 #If frequency is n, window size can be n, 2n , 3n etc. Leave at 0 if you want no overlap
+
+
+#For Boris Processing.py
+BorisPath = '/media/amlan/Data/Thesis Data/BORIS Data/'
+BorisAddPath = '/Event Data/'
+
 
 
 
