61e5dded127b1dd7025fe5df0989c3935f9d6c3b
Amlan-Nayak <68463089+amlan-nayak@users.noreply.github.com>
2022-12-01 13:04:11 +0100

    Edited to GPS extraction files to keep the NaN values

---
+++ GPS_Behaviors.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import os
+from pathlib import Path
+import shutil
+
+#Import the necessary configurations
+#In my case, 4 Different Groups -  ZU_2021_1, ZU_2021_2, NQ_2021_1 and RW_2021_1
+import config
+
+
+ProcessedPath = config.SavePath
+GROUPS = config.GROUPS 
+
+
+for k in GROUPS:
+    
+    SavePath = ProcessedPath + str(k) + '/GPS_Behaviors/'
+    shutil.rmtree(SavePath, ignore_errors=True)
+    os.makedirs(SavePath, exist_ok=True)    
+
+    prediction = os.listdir(ProcessedPath + str(k) + '/Predictions/')
+    paths = os.listdir(ProcessedPath + str(k) + '/GPS/')
+    paths = [i for i in paths if '.ipynb' not in i]
+    dates = [i.split('_')[-1] for i in paths]
+    dates = list(np.unique(dates))
+    individuals = [i.split('_')[1] for i in paths]
+    individuals = list(np.unique(individuals))
+
+    for p in paths:
+        file = p.split('_')
+        for i in prediction:
+            if str(file[1]) in i and str(file[-1]) in i:
+                gps =  pd.read_csv(ProcessedPath + str(k) + '/GPS/'+ p,usecols = ['Timestamp','lat','lon','height-msl'])
+                gps.iloc[:,1:] = gps.iloc[:,1:].rolling(window=3,center=True,min_periods=2).mean()
+                gps =  gps.iloc[1:-1]
+                behavior = pd.read_csv(ProcessedPath + str(k) + '/Predictions/'+ i,usecols = ['Timestamp','Behavior'])
+                final_gps = pd.merge(gps, behavior, on=['Timestamp'],how='inner')
+                final_gps.reset_index(inplace=True,drop=True)
+                final_gps.to_csv(SavePath + '/' + p)  
+
+for k in GROUPS:
+    
+    SavePath = ProcessedPath + str(k) + '/GPS_Days/'
+    shutil.rmtree(SavePath, ignore_errors=True)
+    os.makedirs(SavePath, exist_ok=True) 
+    
+    paths = os.listdir(ProcessedPath + str(k) + '/GPS_Behaviors/')
+    paths = [i for i in paths if '.ipynb' not in i]
+    dates = [i.split('_')[-1] for i in paths]
+    dates = list(np.unique(dates))
+    individuals = [i.split('_')[1] for i in paths]
+    individuals = list(np.unique(individuals))
+    
+    for i in dates:
+        day_data = pd.DataFrame(columns=['Timestamp','lat','lon','height-msl','Behavior','Individual'])
+        for j in paths:
+            if i in j:
+                data = pd.read_csv(ProcessedPath + str(k) + '/GPS_Behaviors/'+ j,index_col=0)
+                data['Individual'] = j.split('_')[1]
+                day_data = pd.concat([day_data,data],axis=0)
+        day_data.reset_index(inplace=True,drop=True)
+        day_data.to_csv(SavePath+'/'+ i)
+    
--- GPS_Extraction.py
+++ GPS_Extraction.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import os
 from pathlib import Path
+import shutil
 
 #Import the necessary configurations
 #In my case, 4 Different Groups -  ZU_2021_1, ZU_2021_2, NQ_2021_1 and RW_2021_1
@@ -27,10 +28,10 @@ def GPS_Extraction(Data):
 #loads GPS data from the combined ACC-GPS files
 def load_GPS_files(FileName,MainPath,Group): 
     File = str(FileName) + '/' + str(FileName) + '.csv' 
-    Data = pd.read_csv(MainPath + str(Group) + AddPath + File,usecols = ['Timestamp','location-lat','location-lon','height-msl']) 
+    Data = pd.read_csv(MainPath + str(Group) + AddPath + File,usecols = ['Timestamp','location-lat','location-lon','satellites','height-msl']) 
     print('\nFile: ',FileName)
     print('Dimensions: ',Data.shape)
-    return Data
+    return Data.loc[Data['satellites']>3]
 
 
 def rename_df(Data):
@@ -41,9 +42,9 @@ def rename_df(Data):
 for i in GROUPS:
 
     paths = os.listdir(MainPath + str(i) + AddPath)
-    os.makedirs(SavePath + str(i) + '/GPS', exist_ok=True) 
+    os.makedirs(SavePath + str(i) + '/GPS/', exist_ok=True) 
     paths = [k for k in paths if 'DS_Store' not in k] #DS_Store not in k is a workaround .DS_Store files in Macs
-
+    
     for y in paths:
         
         Data = load_GPS_files(y,MainPath,i)
@@ -54,11 +55,21 @@ for i in GROUPS:
             pass
         else:
             filename = '_'.join(y.split('_')[0:-1])
+            Data['Timestamp'] = pd.to_datetime(Data['Timestamp'],format="%d/%m/%Y %H:%M:%S.%f")
             Data['Day'] = pd.to_datetime(Data['Timestamp']).dt.date
-
             for key,value in Data.groupby('Day'):
+                    start = sorted(value['Timestamp'].unique())[0]
+                    end = sorted(value['Timestamp'].unique())[-1]
+                    times  = [start+pd.Timedelta(seconds=i) for i in range(int((end-start)/np.timedelta64(1, 's'))+1)]
+                    times = pd.DataFrame(times,columns =['Timestamp'])
+                    value = pd.merge(times, value, on=['Timestamp'],how='outer')
                     value.drop(['Day'],axis=1,inplace=True)
                     value.reset_index(inplace=True)
-                    value.to_csv(SavePath + filename + '_' + str(key))
+                    value.to_csv(SavePath + str(i) +  AddPath + filename + '_' + str(key))
             print("Data Processed for file: ",y)
             print('----')
+
+
+
+
+                
